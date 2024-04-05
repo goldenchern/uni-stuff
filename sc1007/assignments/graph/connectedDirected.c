5c6890896e59cf7044e6fb1c382cb29b33c8ef7f
goldenchern <jiachernkch@gmail.com>
2024-04-05 22:45:54 +0800

    Completed SC1007 assignment 5

---
+++ sc1007/assignments/graph/connectedDirected.c
@@ -0,0 +1,185 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct _listnode
+{
+    int vertex;
+	struct _listnode *next;
+} ListNode;
+typedef ListNode StackNode;
+
+typedef struct _graph{
+    int V;
+    int E;
+    int *visited; //store the visited vertices
+    int **matrix;
+}Graph;
+
+typedef struct _linkedlist
+{
+	int size;
+	ListNode *head;
+} Stack;
+
+// You should not change the prototypes of these functions
+//////STACK///////////////////////////////////////////
+void push(Stack *sPtr, int vertex);
+int pop(Stack *sPtr);
+int peek(Stack s);
+int isEmptyStack(Stack s);
+void removeAllItemsFromStack(Stack *sPtr);
+////GRAPH////////////////////////////////////////////
+void printGraphMatrix(Graph );
+////////////////////////////////////////////
+
+int Connected (Graph);
+
+int main()
+{
+    Graph g;
+    int i,j;
+    
+
+    printf("Enter the number of vertices:\n");
+    scanf("%d",&g.V);
+
+    g.E = 0;
+    g.matrix = (int **)malloc(g.V*sizeof(int *));
+    for(i=0;i<g.V;i++)
+        g.matrix[i] = (int *)malloc(g.V*sizeof(int));
+
+    for(i=0;i<g.V;i++)
+       for(j=0;j<g.V;j++)
+           g.matrix[i][j] = 0;
+	
+	g.visited = (int *) malloc(sizeof(int)*g.V);
+    for(i=0;i<g.V;i++) g.visited[i] = 0;
+    
+    
+    int V1, V2;
+    printf("Enter a directed edge: (press a to stop)\n");
+    while(scanf("%d %d",&V1,&V2)==2)
+    {
+        if(V1>0 && V1<=g.V && V2>0 && V2<=g.V)
+        {
+            g.matrix[V1-1][V2-1] = 1;
+            g.E++;
+        }
+        else
+            break;
+        printf("Enter a directed edge: (press a to stop)\n");
+    }
+    scanf("%*c");
+    
+    
+    int res = Connected(g);
+    if(res == 1)
+        printf("The graph is strongly connected.\n");
+    else
+        printf("The graph is not strongly connected.\n");
+
+    return 0;
+}
+
+int Connected (Graph g)
+{
+    // Frontier list
+    Stack s;
+    s.head = NULL;
+    s.size = 0;
+
+    int temp, unvisitedNeighbours;
+
+    // Do DFS for each vertex in the graph
+    for (int i = 1; i <= g.V; i++) {
+        // Push current vertex to stack and mark visited
+        push(&s, i);
+        g.visited[i-1] = 1;
+
+        while (!isEmptyStack(s)) {
+            // Reset flag
+            unvisitedNeighbours = 0;    
+            temp = peek(s);    
+
+            // For each of the other vertices in the graph
+            for (int j = 0; j < g.V; j++) {
+                // Check if adjacent and unvisited
+                if (g.matrix[temp-1][j] && !g.visited[j]) {
+                    // Push to stack and mark visited
+                    push(&s, j+1);
+                    g.visited[j] = 1;
+                    unvisitedNeighbours = 1;
+                    break;
+                }
+            }
+
+            // If no adjacent unvisited vertices, pop from stack
+            if (!unvisitedNeighbours) {
+                pop(&s);
+            }
+        }
+
+        // Check if all vertices can be reached from current vertex
+        for (int k = 0; k < g.V; k++) {
+            if (!g.visited[k]) {
+                return 0;
+            }
+            // Mark each node unvisited for next search iteration
+            g.visited[k] = 0;
+        }
+    }
+    
+    return 1;
+}
+
+
+void printGraphMatrix(Graph g)
+{
+    int i,j;
+
+    for(i=0;i<g.V;i++){
+        for(j=0;j<g.V;j++)
+            printf("%d\t",g.matrix[i][j]);
+        printf("\n");
+    }
+
+}
+
+void push(Stack *sPtr, int vertex)
+{
+	StackNode *newNode;
+    newNode= malloc(sizeof(StackNode));
+    newNode->vertex = vertex;
+    newNode->next = sPtr->head;
+    sPtr->head = newNode;
+    sPtr->size++;
+}
+
+int pop(Stack *sPtr)
+{
+    if(sPtr==NULL || sPtr->head==NULL){
+        return 0;
+    }
+    else{
+       StackNode *temp = sPtr->head;
+       sPtr->head = sPtr->head->next;
+       free(temp);
+       sPtr->size--;
+       return 1;
+    }
+}
+
+int isEmptyStack(Stack s)
+{
+     if(s.size==0) return 1;
+     else return 0;
+}
+
+int peek(Stack s){
+    return s.head->vertex;
+}
+
+void removeAllItemsFromStack(Stack *sPtr)
+{
+	while(pop(sPtr));
+}
